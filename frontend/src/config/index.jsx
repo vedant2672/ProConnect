9b24c7a72e4e17d7c71fa0997e60db8fe1a7ff99
Vedant Singh <vedantsingh2672@gmail.com>
2025-08-16 00:39:05 +0530

    Frontend No Error

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://proconnect-b8ci.onrender.com/";
+export const BASE_URL = "https://proconnect-b8ci.onrender.com";
 
 export const clientServer = axios.create({
   baseURL: BASE_URL,
